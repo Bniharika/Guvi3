db756c544ba0edb284a266b2e818985828ce259d
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-07-27 15:08:42 +0530

    Create Repeat.java

---
+++ Repeat.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Repeat1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a value");
+		long N=sc.nextLong();
+		for(int i=1;i<N;i++)
+		{
+			System.out.println("hello");
+		}
+		
+	}
+}
