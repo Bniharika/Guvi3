4e545fa585883bcc4b7e9351c3a30d93691d0d3d
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-08 11:38:54 +0530

    Create Kabali81.java

---
+++ Kabali81.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+				Scanner sc=new Scanner(System.in);
+				int num1=sc.nextInt();
+				int num2=sc.nextInt();
+				int num=Math.abs(num1-num2);
+				System.out.println(num);
+	}
+}
