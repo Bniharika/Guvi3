df8d6d327c1e5641b08df90394299ce0840d1174
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-09 13:56:56 +0530

    Create Averageofnumbers.java

---
+++ Averageofnumbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int c=sc.nextInt();
+			int d=sc.nextInt();
+			int e=sc.nextInt();
+			int sum=0;
+			sum=(a+b+c+d+e)/n;
+			System.out.print(sum);
+	}
+}
