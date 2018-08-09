d9a50b34a0e63c25e56396f906b7b4c2f71c1f0d
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-09 11:39:37 +0530

    Create Multipules.java

---
+++ Multipules.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner sc=new Scanner(System.in);
+			int m=sc.nextInt();
+			for(int i=1;i<6;i++)
+			{
+				int mul=m*i;
+				System.out.print(mul+" ");
+			}
+	}
+}
