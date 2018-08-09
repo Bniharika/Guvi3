480f5b2e5232ccabd3c5d9172a149eccbb954c21
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-09 12:35:09 +0530

    Create Stringandnumberrepeats.java

---
+++ Stringandnumberrepeats.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			System.out.println(s);
+		}
+		
+	}
+}
