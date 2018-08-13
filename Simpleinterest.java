2b216980575feba5ecca658f0969f5233bf785fc
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-13 10:44:54 +0530

    Create Simpleinterest.java

---
+++ Simpleinterest.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int p=sc.nextInt();
+	    int t=sc.nextInt();
+	    int r=sc.nextInt();
+	    int si;
+	    si=(p*t*r)/100;
+	    System.out.print(si);
+	}
+}
