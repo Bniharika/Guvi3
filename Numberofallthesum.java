c790110a873d9f3141e28c859d073663bdb4fa9f
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-13 10:35:57 +0530

    Create Numberofallthesum.java

---
+++ Numberofallthesum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int m=sc.nextInt();
+		int n=sc.nextInt();
+		int o=sc.nextInt();
+		int p=sc.nextInt();
+		int q=sc.nextInt();
+		int r=n+o+p+q;
+		System.out.print(r);
+		
+	}
+}
