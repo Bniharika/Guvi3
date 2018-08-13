44a6d2158dacdd78dd43bd901094d025566f6087
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-13 09:42:39 +0530

    Create Multipleof13.java

---
+++ Multipleof13.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int m=sc.nextInt();
+		if(m%13==0)
+			System.out.println("yes");
+		else
+			System.out.print("no");
+	}
+}
