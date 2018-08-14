47ed04e41d719334d86b30d9fdd0cb7df4035bfa
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-14 11:01:43 +0530

    Update Twonumbersbetweenarmstrong.c

--- Twonumbersbetweenarmstrong.c
+++ Twonumbersbetweenarmstrong.c
@@ -11,31 +11,22 @@ int main()
     {
         temp2 = i;
         temp1 = i;
-
-        // number of digits calculation
         while (temp1 != 0)
         {
             temp1 /= 10;
             ++n;
         }
-
-        // result contains sum of nth power of its digits
         while (temp2 != 0)
         {
             remainder = temp2 % 10;
             result += pow(remainder, n);
             temp2 /= 10;
         }
-
-        // checks if number i is equal to the sum of nth power of its digits
         if (result == i) {
             printf("%d ", i);
         }
-
-        // resetting the values to check Armstrong number for next iteration
         n = 0;
         result = 0;
-
     }
     return 0;
 }
