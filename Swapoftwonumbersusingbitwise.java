ebbbd67cd17e002ac5c39bfd24b741b0f0863a4b
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-17 11:19:33 +0530

    Update Swapoftwonumbersusingbitwise.java

--- Swapoftwonumbersusingbitwise.java
+++ Swapoftwonumbersusingbitwise.java
@@ -12,12 +12,10 @@ class Ideone
                     Scanner sc=new Scanner(System.in);
                     int a=sc.nextInt();
                     int b=sc.nextInt();
-                    System.out.println(a);
-                    System.out.println(b);
+                    System.out.println(a+" "+b);
                     a = a ^ b;
                     b = a ^ b;
                     a = a ^ b; 
-                   System.out.println(a);
-                   System.out.println(b);
+                   System.out.println(a+" "+b);
 	}
 }
