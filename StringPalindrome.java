a9876c69da16d55d2b9981a653fd0a1a48fada62
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-23 10:24:27 +0530

    Create StringPalindrome.java

---
+++ StringPalindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String rev="";
+		 int length = str.length();
+ 
+      for ( int i = length - 1; i >= 0; i-- )
+         rev = rev + str.charAt(i);
+ 
+      if (str.equals(rev))
+         System.out.println("yes");
+      else
+         System.out.println("no");
+ 
+	}
+}
