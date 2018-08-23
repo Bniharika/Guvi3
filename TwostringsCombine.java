da23840ce18edac7605767d69b056002d6e81161
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-23 11:03:59 +0530

    Create TwostringsCombine.java

---
+++ TwostringsCombine.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String st=sc.nextLine();
+		String str=sc.nextLine();
+		System.out.print(st);
+		System.out.print(str);
+	}
+}
