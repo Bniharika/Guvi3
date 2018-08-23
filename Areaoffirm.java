21e71f443209de968f0fb6da78c0a76ccdb93d11
Bniharika <41765599+Bniharika@users.noreply.github.com>
2018-08-23 12:07:03 +0530

    Create Areaoffirm.java

---
+++ Areaoffirm.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		double l=sc.nextDouble();
+		double h=sc.nextDouble();
+	            double m=l * h;
+		System.out.format("%.5f",m);
+	}
+}
